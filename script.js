0f998b46b0a01efd8ee7f948389c7caf0a9a8594
Ruthrapathi-murugan <153907596+Ruthrapathi-murugan@users.noreply.github.com>
2024-02-25 02:28:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-
-function foo(){
+function foo() {
     var first = document.getElementById("firstname").value;
     console.log(`Firstname:${first}`);
 
     var second = document.getElementById("middlename").value;
     console.log(`Middlename:${second}`);
+    
     var last = document.getElementById("lastname").value;
     console.log(`Lastname: ${last}`);
 
@@ -13,8 +13,24 @@ function foo(){
 
     var password = document.getElementById("password").value;
     console.log(`Password: ${password}`);
-}
 
+    // Displaying input values in spans
+    var firstNameSpan = document.createElement("span");
+    firstNameSpan.innerHTML = `First Name: ${first}`;
+    document.body.append(firstNameSpan);
+
+    var middleNameSpan = document.createElement("span");
+    middleNameSpan.innerHTML = `Middle Name: ${second}`;
+    document.body.append(middleNameSpan);
+
+    var lastNameSpan = document.createElement("span");
+    lastNameSpan.innerHTML = `Last Name: ${last}`;
+    document.body.append(lastNameSpan);
+
+    var emailSpan = document.createElement("span");
+    emailSpan.innerHTML = `Email: ${email}`;
+    document.body.append(emailSpan);
+}
 
 function labelcreate(tagname, attrname, attrvalue, content) {
     var ele = document.createElement(tagname);
@@ -24,8 +40,7 @@ function labelcreate(tagname, attrname, attrvalue, content) {
 }
 
 function breakcreate() {
-    var ele = document.createElement("br");
-    return ele;
+    return document.createElement("br");
 }
 
 function inputcreate(tagname, attr1name, attr1value, attr2name, attr2value) {
@@ -37,41 +52,40 @@ function inputcreate(tagname, attr1name, attr1value, attr2name, attr2value) {
 
 // Creating input fields for first name
 var firstnamelabel = labelcreate("label", "for", "firstname", "First Name:");
-var firstnamebreak = breakcreate("br");
+var firstnamebreak = breakcreate();
 var firstnameinput = inputcreate("input", "type", "text", "id", "firstname");
 
 // Creating input fields for middle name
 var middlenamelabel = labelcreate("label", "for", "middlename", "Middle Name:");
-var middlenamebreak = breakcreate("br");
+var middlenamebreak = breakcreate();
 var middlenameinput = inputcreate("input", "type", "text", "id", "middlename");
 
 // Creating input fields for last name
 var lastnamelabel = labelcreate("label", "for", "lastname", "Last Name:");
-var lastnamebreak = breakcreate("br");
+var lastnamebreak = breakcreate();
 var lastnameinput = inputcreate("input", "type", "text", "id", "lastname");
 
-
-var emaillabel = labelcreate("label", "for", "Email", "Email");
-var emailbreak = breakcreate("br");
-var emailinput = inputcreate("input", "type", "mail", "id", "email");
+var emaillabel = labelcreate("label", "for", "email", "Email");
+var emailbreak = breakcreate();
+var emailinput = inputcreate("input", "type", "email", "id", "email");
 
 // Creating input fields for password
 var passwordlabel = labelcreate("label", "for", "password", "Password:");
-var passwordbreak = breakcreate("br");
+var passwordbreak = breakcreate();
 var passwordinput = inputcreate("input", "type", "password", "id", "password");
 
 //Submit
 var submitButton = document.createElement("button");
 submitButton.textContent = "Submit";
-submitButton.setAttribute("type", "submit");
+submitButton.setAttribute("type", "button"); // Changed type from "submit" to "button"
 submitButton.addEventListener("click", foo);
 
-
 // Appending all elements to the body
-document.body.append(firstnamelabel, firstnamebreak, firstnameinput);
-document.body.append(middlenamelabel, middlenamebreak, middlenameinput);
-document.body.append(lastnamelabel, lastnamebreak, lastnameinput);
-document.body.append(emaillabel, emailbreak, emailinput);
-document.body.append(passwordlabel, passwordbreak, passwordinput);
-document.body.append(submitButton);
-
+var form = document.createElement("form");
+form.append(firstnamelabel, firstnamebreak, firstnameinput);
+form.append(middlenamelabel, middlenamebreak, middlenameinput);
+form.append(lastnamelabel, lastnamebreak, lastnameinput);
+form.append(emaillabel, emailbreak, emailinput);
+form.append(passwordlabel, passwordbreak, passwordinput);
+form.append(submitButton);
+document.body.append(form);
